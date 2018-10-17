7f93d67f1130187c5fd134f3a03acb2bf6eacb06
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-17 07:58:27 -0400

    aula api express 02

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,9 +2,23 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('/home/diego-felipe/repositorios/cursoJSCod3r/node/projeto/src/bancodeDados.js')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // Converter para JSON automaticamente
+    // res.send({nome: 'Notebook', preco: 123.45}) // Converter para JSON automaticamente
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req,res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {

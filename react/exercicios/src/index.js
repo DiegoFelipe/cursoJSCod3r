71b90b6846d0a0767e8fc092559df8b90c9fc079
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-25 08:10:57 -0400

    Component com classe

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom'
 
 import Primeiro from './componentes/Primeiro'
 import BomDia from './componentes/BomDia'
+import Saudacao from './componentes/Saudacao'
 
 ReactDOM.render(<Primeiro/>, document.getElementById('root'))
 ReactDOM.render(<BomDia nome = "diego" idade = {27}/>, document.getElementById('root'))
+
+ReactDOM.render(
+    <div>
+        <Saudacao tipo="Bom dia" nome="João" />
+    </div>
+, document.getElementById('root'))

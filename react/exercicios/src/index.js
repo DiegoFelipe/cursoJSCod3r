80d9e0fe6490a358ca081418a178730a34949c98
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-25 12:20:27 -0400

    Aula de Componentes Múltiplos

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import Primeiro from './componentes/Primeiro'
 import BomDia from './componentes/BomDia'
 import Saudacao from './componentes/Saudacao'
+import { BoaTarde, BoaNoite } from './componentes/Multiplos'
 
 ReactDOM.render(<Primeiro/>, document.getElementById('root'))
 ReactDOM.render(<BomDia nome = "diego" idade = {27}/>, document.getElementById('root'))
@@ -11,5 +12,7 @@ ReactDOM.render(<BomDia nome = "diego" idade = {27}/>, document.getElementById('
 ReactDOM.render(
     <div>
         <Saudacao tipo="Bom dia" nome="João" />
+        <BoaTarde nome="diego" />
+        <BoaNoite nome="diego" />
     </div>
 , document.getElementById('root'))

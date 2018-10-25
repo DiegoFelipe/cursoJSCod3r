c34d59e73d39ea728bd95c3f51d5d765da3cfb47
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-25 07:40:53 -0400

    Componente com propriedade

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
 
 ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+ReactDOM.render(<BomDia nome = "diego" idade = {27}/>, document.getElementById('root'))

2d21327b75ec35ccfd414efe91efb2d9da9346ff
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-26 08:15:25 -0400

    Aula pai2

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -7,6 +7,8 @@ import Saudacao from './componentes/Saudacao'
 import { BoaTarde, BoaNoite } from './componentes/Multiplos'
 
 import Pai from './componentes/Pai'
+import Pai2 from './componentes/Pai2'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(<Primeiro/>, document.getElementById('root'))
 ReactDOM.render(<BomDia nome = "diego" idade = {27}/>, document.getElementById('root'))
@@ -17,5 +19,11 @@ ReactDOM.render(
         <BoaTarde nome="diego" />
         <BoaNoite nome="diego" />
         <Pai nome="Paulo" sobrenome="Silva" />
+
+        <Pai2 nome="Diego" sobrenome="Felipe">
+            <Filho nome="João" sobrenome="Felipe" />
+            <Filho nome="Marcos" sobrenome="Felipe" />
+            <Filho nome="Laila" sobrenome="Souza" />
+        </Pai2>
     </div>
 , document.getElementById('root'))

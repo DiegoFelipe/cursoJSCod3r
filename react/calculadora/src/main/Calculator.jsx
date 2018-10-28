7a8a53e1ee37ada53bb49632b0d1f1975bfc2756
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-27 20:42:20 -0400

    lógica da calculadora 1-1

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -33,14 +33,26 @@ export default class Calculator extends Component  {
   }
 
   addDigit(n) {
-    if (n == '.' && this.state.displayValue.includes('.')) return
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
 
-    const clearDisplay = this.state.displayValue = '0'
+    const clearDisplay = this.state.displayValue === '0'
                         || this.state.clearDisplay
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
 
-    this.setState({displayValue, clearDisplay: false}) 
+    this.setState({displayValue, clearDisplay: false})
+
+    if(n !==  '.') {
+        const i = this.state.current
+        const newValue = parseFloat(displayValue)
+        const values = [...this.state.values]
+
+        values[i] = newValue
+        this.setState({ values })
+        console.log(values)
+    }
   }
 
   render(){

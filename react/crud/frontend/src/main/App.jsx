b3f3712f8bc779626473d34895fab812530d08b3
Diego Felipe <diegofelipe.14@gmail.com>
2018-10-31 08:08:29 -0400

    Projeto frontend e backend do crud de usuários

---
+++ react/crud/frontend/src/main/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+import Main from '../components/template/Main'
+import Logo from '../components/template/Logo'
+import Footer from '../components/template/Footer'
+import Nav from '../components/template/Nav'
+
+export default props =>
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main />
+        <Footer />
+    </div>

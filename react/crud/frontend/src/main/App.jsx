07863f710bc60e03c8a9e82440a61b262b2566aa
Diego Felipe <diegofelipe.14@gmail.com>
2018-11-12 22:25:51 -0400

    Creating routes component

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -2,16 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 
+import Routes from './Routes'
 import Home from '../components/home/Home'
 import Logo from '../components/template/Logo'
 import Footer from '../components/template/Footer'
 import Nav from '../components/template/Nav'
 
 export default props =>
-    <div className="app">
+    <HashRouter>
+      <div className="app">
         <Logo />
         <Nav />
-        <Home />
+        <Routes />
         <Footer />
-    </div>
+      </div>
+    </HashRouter>
